24bf61fe330b9fa48bbd705b11476a857fd9e459
Arif Khan <aarifkhan873287@gmail.com>
2024-04-12 23:07:30 +0530

    Bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Hero from "./componets/Hero";
 import Banner from "./componets/Banner";
 import Benefits from "./componets/Benefits";
 import Applications from "./componets/Applications";
+import CustomCarousel from "./componets/CustomCarousel";
 
 export default function Home() {
   return (
     <>
     <Hero/>
+    <CustomCarousel/>
     <Banner/>
     <Benefits/>
     <Applications/>
