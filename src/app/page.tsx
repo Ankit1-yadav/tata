13650755bc9bea5d933cb6131abc30a21f46b398
Ankit1-yadav <ankit.runtime@gmail.com>
2024-04-16 10:58:44 +0530

    test1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,20 @@
-import Image from "next/image";
-import Hero from "./componets/Hero";
+
 import Banner from "./componets/Banner";
 import Benefits from "./componets/Benefits";
 import Applications from "./componets/Applications";
-import CustomCarousel from "./componets/CustomCarousel";
 import NewHero from "./componets/NewHero";
-import Sections from "./componets/Sections";
-import Projects from "./componets/Projects";
-// 
 
 export default function Home() {
   return (
     <>
-    {/* <Hero/> */}
+ 
     <NewHero/>
-    {/* <CustomCarousel/> */}
+  
     <Banner/>
-    {/* <Projects/> */}
+   
     <Benefits/>
 
-    {/* <Sections/> */}
+    
     <Applications/>
     </>
   );
