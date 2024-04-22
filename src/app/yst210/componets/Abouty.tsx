3914a6530a35460088211adde649a5b2f537ef63
Ankit1-yadav <ankit.runtime@gmail.com>
2024-04-22 19:38:45 +0530

    new again

---
+++ src/app/yst210/componets/Abouty.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Abouty = () => {
+  return (
+    <div className='bg-blue-500'>
+      <div className="container">
+    <div className="row">
+        <div className="section1content mt-5">
+            <p className="products-para">Tata Structura, in the retail segment, has been used in home building applications
+                like gates, railings, fences, grilles, pergolas, and roof sheds. It is also used in shed applications
+                like cattle, poultry, barn, SME enclosures, etc., and enclosures of religious, educational, and
+                commercial institutions</p>
+            <p className="products-para">Tata Structura Yst 210 steel tubes possess a yield strength of over 210 MPa and
+                ultimate tensile strength of over 330 MPa, along with a high elongation of over 20%. The cross sections
+                of Tata Structura’s circular, square, and rectangular hollow steel sections have superior geometric
+                properties, providing it excellent structural quality amongst the commonly available construction
+                products in the market.</p>
+            <p className="products-para">The combined advantage of high-quality steel tubes with consistent yield strength
+                and superior geometric properties helps save over 25% of material wastage and is economical when
+                compared to open sections like angles and channels etc. The surface of Tata Structura steel tubes is
+                also ideal to achieve a smooth surface finish that provides a superior look and feel for the end
+                application.</p>
+        </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Abouty
--- src/app/yst210/page.tsx
+++ src/app/yst210/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from '../componets/Hero'
+import Product from '../AboutUs/componets/Product'
+import Abouty from './componets/Abouty'
 
 const page = () => {
   return (
     <div>
     {/* <Hero/> */}
     <Hero title="TATA STRUCTURA YST 210" subtitle="Tata Structura YST 210 is a brand of galvanized steel hollow sections from the house of Tata Steel." img="/yst210.jpg" />
-
+    <Abouty/>
+    <Product/>
     </div>
   )
 }
